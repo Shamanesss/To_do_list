c70b1c7dea801cf7360c465a42e8762c74f3ef7d
Shamanesss <mentulbenodorna@gmail.com>
2023-04-19 21:53:11 +0200

    añadir post y get

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,37 @@
 <template>
- 
-  <router-view/>
+  <nav>
+    <router-link to="/">Profesional</router-link> |
+
+    <router-link to="/personal">Personal</router-link>
+  </nav>
+  <router-view />
 </template>
 
 <style>
+html {
+  margin: 0;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  background-image: url("../src/assets/todolist.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+  box-sizing: border-box;
+  min-height: 100vh;
+}
 
+nav {
+  padding: 30px;
+}
+nav a {
+  font-weight: bold;
+  color: #204643;
+}
+nav a.router-link-exact-active {
+  color: white;
+}
 </style>
--- src/components/TarjetaTareas.vue
+++ src/components/TarjetaTareas.vue
@@ -1,20 +1,14 @@
 <template>
   <div class="contenedor">
     <TarjetaInput @nueva-tarea="(tarea) => imprimirTarea()" />
-<div class="tareas">
-
-</div>
+    <div class="tareas"></div>
   </div>
 </template>
 
 <script setup>
-import TarjetaInput from '@/components/TarjetaInput.vue'
-function imprimirTarea(){
-  nuevaTarea.value.push
-}
+import TarjetaInput from "@/components/TarjetaInput.vue";
+function imprimirTarea() {}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-
-</style>
+<style scoped></style>
