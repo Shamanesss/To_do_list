e0e0cc48fdd3eddd5bffde5676cc2017cd615fd5
Shamanesss <mentulbenodorna@gmail.com>
2023-04-20 13:04:03 +0200

    post

--- src/components/TarjetaInput.vue
+++ src/components/TarjetaInput.vue
@@ -3,20 +3,35 @@
 <form action="" @submit.prevent="introducirDatos">
     <h2>Crear Tareas</h2>
 <input type="text" v-model="tarea" >
+<p class="error"></p>
 </form>
     </div>
 </template>
 
 <script setup>
- import { ref } from "vue"
+ import axios from "axios";
+import { ref } from "vue"
  import { defineEmits } from "vue"
  const tarea = ref("")
+ const error= ref("")
 const emits = defineEmits (["nuevaTarea"])
-function introducirDatos(){
-    if(tarea.value !== ''){
+const introducirDatos=async()=>{
+    try{
+    if(tarea.value === ''){
+        error.value ="introduce una tarea";
+        return;
+    }
+    const response = await axios.post("http://localhost:3000/tareas", {
+        tarea: tarea.value,
+    });
+    console.log(response);
+}   catch (error){
+    console.log(error)
+    error.value= "Se ha producido un error"
+}
         emits("nuevaTarea", tarea)
         tarea.value = ''
-    }
+    
 }
 </script>
 
